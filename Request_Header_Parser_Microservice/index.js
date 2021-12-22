ad7c58fe7d65edd2f1a90f1bedae9915de8b1753
Duoc Dev <buithanhduoc2018@gmail.com>
2021-12-22 22:44:02 +0700

    Request Header Parser Microservice

---
+++ Request_Header_Parser_Microservice/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.use(cors());
+app.get('/', (req, res) => { 
+    console.log(req.headers);
+    const header_Parser = {
+        "ipaddress": req.ip,
+        "language": req.headers["accept-language"],
+        "software": req.headers["user-agent"]
+    }
+    res.json(header_Parser);
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
